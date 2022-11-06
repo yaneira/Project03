c7f92d4ef1d365c3db4b9162951e6dbca80fe755
yaneira <98625065+yaneira@users.noreply.github.com>
2022-11-06 13:40:20 -0500

    corected

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+menu.addEventListener('click', function(){
+  menuLinks.classList.toggle('active');
+  menuLinks.classList.toggle('active');
+});
